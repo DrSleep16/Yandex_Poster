86eb36d7fbad19f350501dd01bd4d1b52f486b82
Дмитрий <Dr.Sleep1995@gmail.com>
2025-07-09 23:25:14 +0300

    Add model for images

--- where_to_go/places/models.py
+++ where_to_go/places/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Place(models.Model):
     title = models.CharField(max_length=200, verbose_name='Название')
     short_description = models.TextField(verbose_name='Краткое описание', blank=True)
@@ -9,3 +10,15 @@ class Place(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class PlaceImage(models.Model):
+  place = models.ForeignKey(Place, on_delete=models.CASCADE, related_name='images', verbose_name='Место')
+  image = models.ImageField(upload_to='places_images', verbose_name='Картинка')
+  order = models.PositiveIntegerField(default=0, verbose_name='Порядок')
+
+  class Meta:
+    ordering = ['order']
+
+  def __str__(self):
+    return f"{self.place.title} - {self.order}"
--- where_to_go/where_to_go/settings.py
+++ where_to_go/where_to_go/settings.py
@@ -1,3 +1,5 @@
+import os
+
 from pathlib import Path
 
 
@@ -82,7 +84,8 @@ USE_I18N = True
 USE_TZ = True
 
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [ BASE_DIR / "static"]
-
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
