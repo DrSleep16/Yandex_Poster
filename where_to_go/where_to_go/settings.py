7ba76ee685e71f83c7b7398047e254f99496f7c9
Дмитрий <Dr.Sleep1995@gmail.com>
2025-07-09 22:56:58 +0300

    Create places app

--- where_to_go/where_to_go/settings.py
+++ where_to_go/where_to_go/settings.py
@@ -17,6 +17,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'places'
 ]
 
 MIDDLEWARE = [
@@ -82,9 +83,6 @@ USE_TZ = True
 
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    "/var/www/static/",
-]
+STATICFILES_DIRS = [ BASE_DIR / "static"]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
